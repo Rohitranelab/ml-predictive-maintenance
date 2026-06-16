933687523ddd55e92f4cebcfb7eee68e39bdd000
Rohit <ranerohit996@gmail.com>
2026-06-16 19:30:37 +0530

    requirements are added

---
+++ src/constants/__init__.py
@@ -0,0 +1,69 @@
+import os
+from datetime import date
+
+# For MongoDB Connection
+DATABASE_NAME = "predictive_ml"
+COLLECTION_NAME = "Predictive-Data"
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = ""
+ARTIFACT_DIR: str = "artifact"
+
+MODEL_FILE_NAME = "predictive_maintenance_model.pkl"
+
+TARGET_COLUMN = "Machine failure"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+FILE_NAME: str = "ai4i2020.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+'''
+Data Ingestion related constant start with DATA_INJESTION VAR NAME
+'''
+DATA_INJESTION_COLLECTION_NAME: str = "Predictive-Data"
+DATA_INJESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIT: str = "ingested"
+DATA_INGESTION_TRAIN_AND_TEST_SPLIT_RATIO: float = 0.2
+
+'''
+DATA VALIDATION related constant start with DATA_VALIDATION VAR NAME
+'''
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALDATION_REPORT_FILE_NAME: str = "report.yaml"
+
+'''
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+'''
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMED_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+'''
+Model Trainer related constant start with MODEL_TRAINER VAR NAME
+'''
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "predictive_maintenance_model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+MODELTRAINER_N_ESTIMATORS = 100
+MODEL_TRAINER_MIN_SAMPLES_SPLIT: int = 2
+MOEDL_TRAINER_MIN_SAMPLES_LEAF: int = 3
+MIN_SAMPLES_SPLIT_MAX_DEPTH: int = None
+MIN_SAMPLES_SPLIT_CRITERATION: str = 'entropy'
+MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 42
+
+'''
+Model Evaluation related constant
+'''
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "predictive-model"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 5000
---
+++ src/entity/config_entity.py
@@ -0,0 +1,67 @@
+import os
+from src.constants import *
+from dataclasses import dataclass
+from datetime import datetime
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INJESTION_DIR_NAME)
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIT, TRAIN_FILE_NAME)
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIT, TEST_FILE_NAME)
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_AND_TEST_SPLIT_RATIO
+    collection_name: str = DATA_INJESTION_COLLECTION_NAME
+
+@dataclass
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
+    validation_report_file_path: str = os.path.join(data_validation_dir, DATA_VALDATION_REPORT_FILE_NAME)
+
+@dataclass
+class DataTransformationConfig:
+    data_transformation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_TRANSFORMATION_DIR_NAME)
+    transformation_train_file_path: str = os.path.join(data_transformation_dir, DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+                                                       TRAIN_FILE_NAME.replace('csv', 'npy'))
+    transformation_test_file_path: str = os.path.join(data_transformation_dir, DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+                                                      TEST_FILE_NAME.replace('csv', 'npy'))
+    transformed_object_file_path: str = os.path.join(data_transformation_dir, DATA_TRANSFORMED_TRANSFORMED_OBJECT_DIR,
+                                                     PREPROCESSING_OBJECT_FILE_NAME)
+    
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_dir: str = os.path.join(training_pipeline_config.artifact_dir, MODEL_TRAINER_DIR_NAME)
+    trained_model_file_path: str = os.path.join(model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR, MODEL_FILE_NAME)
+    excepted_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
+    model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
+    _n_estimators = MODELTRAINER_N_ESTIMATORS
+    _min_samples_split = MODEL_TRAINER_MIN_SAMPLES_SPLIT
+    _min_samples_leaf = MOEDL_TRAINER_MIN_SAMPLES_LEAF
+    _max_depth  = MIN_SAMPLES_SPLIT_MAX_DEPTH
+    _criteration = MIN_SAMPLES_SPLIT_CRITERATION
+    _random_state = MIN_SAMPLES_SPLIT_RANDOM_STATE
+
+@dataclass
+class ModelEvaluationConfig:
+    changed_threshold_score: float = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILE_NAME
+
+@dataclass
+class ModelPusherConfig:
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILE_NAME
+
+@dataclass
+class PredictionMaintenanceConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
