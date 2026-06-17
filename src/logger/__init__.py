56e733179b29d801d4a06684bad22b2a33d434c5
Rohit <ranerohit996@gmail.com>
2026-06-17 12:45:28 +0530

    update logger file

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,22 +1,20 @@
 import logging
 import os
 from logging.handlers import RotatingFileHandler
-from from_root import from_root
 from datetime import datetime
 
 # Constants for logs configuration
-LOG_DIRS = 'logs'
+LOG_DIRS = '/tmp/logs'
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-MAX_LOG_SIZE = 5 * 1024 * 1024 # 5 MB
+MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
 BACKUP_COUNT = 3
 
-# Construct log file path
-log_dir_path = os.path.join(from_root(), LOG_DIRS)
-os.makedirs(log_dir_path, exist_ok = True)
+# Construct log file path - use /tmp for Vercel serverless
+log_dir_path = os.path.join(LOG_DIRS)
+os.makedirs(log_dir_path, exist_ok=True)
 log_file_path = os.path.join(log_dir_path, LOG_FILE)
 
 def configure_logger():
-
     # Create a custom logger
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
@@ -25,7 +23,7 @@ def configure_logger():
     formatter = logging.Formatter("[%(asctime)s - %(name)s - %(levelname)s - %(message)s]")
 
     # File handler with rotation
-    file_handler = RotatingFileHandler(log_file_path, maxBytes = MAX_LOG_SIZE, backupCount = BACKUP_COUNT)
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_COUNT)
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.DEBUG)
 
