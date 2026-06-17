ef171eeddf25cccc680b20a0c85d3ac45ec7d091
Rohit <ranerohit996@gmail.com>
2026-06-17 19:33:28 +0530

    change minor value in logger file

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -10,7 +10,7 @@ MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
 BACKUP_COUNT = 3
 
 # Construct log file path - use /tmp for Vercel serverless
-log_dir_path = os.path.join(LOG_DIRS)
+log_dir_path = '/tmp/logs'
 os.makedirs(log_dir_path, exist_ok=True)
 log_file_path = os.path.join(log_dir_path, LOG_FILE)
 
